c5fbb428ae085f3a416168c9e2191117d87fa763
ryder <sky997121@gmail.com>
2021-12-30 15:00:28 +0800

    continue upload

--- js/customized/randomBg.js
+++ js/customized/randomBg.js
@@ -1,10 +1,1 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var elementById = document.getElementsByClassName("full_page").item(0);
-    if (elementById != null) {
-        // 取0到10的随机数
-        var number = Math.random() * 10;
-        var bgNumber = Math.ceil(number);
-        var bgPath = "/img/index_bg/bg" + bgNumber + ".png";
-        elementById.style.backgroundImage = "url('" + bgPath + "')";
-    }
-});
+"use strict";document.addEventListener("DOMContentLoaded",function(){var e,t=document.getElementsByClassName("full_page").item(0);null!=t&&(e=10*Math.random(),e=Math.ceil(e),t.style.backgroundImage="url('"+("https://cdn.jsdelivr.net/gh/sky04290/io_rydersite@1.0.1/img/index_bg/bg"+e+".png")+"')")});
